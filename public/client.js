156f15553f3446f59581559fa6818e1dea0bb5a0
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-02-06 21:10:48 -0800

    adding routes in app file and new code in test file

--- public/client.js
+++ public/client.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const addPerson = document.getElementById("addPerson");
-  const getPerson = document.getElementById("getPerson");
-  const listPeople = document.getElementById("listPeople");
-  const name = document.getElementById("name");
-  const age = document.getElementById("age");
-  const index = document.getElementById("index");
-  const result = document.getElementById("result");
-  addPerson.addEventListener("click", async (event) => {
+document.addEventListener('DOMContentLoaded', () => {
+  const addPerson = document.getElementById('addPerson');
+  const getPerson = document.getElementById('getPerson');
+  const listPeople = document.getElementById('listPeople');
+  const name = document.getElementById('name');
+  const age = document.getElementById('age');
+  const index = document.getElementById('index');
+  const result = document.getElementById('result');
+  addPerson.addEventListener('click', async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch("/api/v1/people", {
-        method: "POST",
+      const response = await fetch('/api/v1/people', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({ name: name.value, age: Number(age.value) }),
       });
@@ -22,11 +22,11 @@ document.addEventListener("DOMContentLoaded", () => {
       result.textContent = err.message;
     }
   });
-  listPeople.addEventListener("click", async (event) => {
+  listPeople.addEventListener('click', async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch("/api/v1/people", {
-        headers: { "Content-Type": "application/json" },
+      const response = await fetch('/api/v1/people', {
+        headers: { 'Content-Type': 'application/json' },
       });
       const data = await response.json();
       result.textContent = JSON.stringify(data);
@@ -34,13 +34,13 @@ document.addEventListener("DOMContentLoaded", () => {
       result.textContent = err.message;
     }
   });
-  getPerson.addEventListener("click", async (event) => {
+  getPerson.addEventListener('click', async (event) => {
     event.preventDefault();
     const index1 = encodeURIComponent(index.value);
-    console.log("index 1 is ", index1);
+    console.log('index 1 is ', index1);
     try {
       const response = await fetch(`/api/v1/people/${index1}`, {
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Content-Type': 'application/json' },
       });
       const data = await response.json();
       result.textContent = JSON.stringify(data);
--- tests/puppeteer.js
+++ tests/puppeteer.js
@@ -1,7 +1,7 @@
-const puppeteer = require("puppeteer");
-require("dotenv").config();
-const chai = require("chai");
-const { server } = require("../app");
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+const chai = require('chai');
+const { server } = require('../app');
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -10,14 +10,14 @@ function sleep(ms) {
 chai.should();
 
 (async () => {
-  describe("Functional Tests with Puppeteer", function () {
+  describe('Functional Tests with Puppeteer', function () {
     let browser = null;
     let page = null;
     before(async function () {
       this.timeout(5000);
       browser = await puppeteer.launch();
       page = await browser.newPage();
-      await page.goto("http://localhost:3000");
+      await page.goto('http://localhost:3000');
     });
     after(async function () {
       this.timeout(5000);
@@ -25,14 +25,14 @@ chai.should();
       server.close();
       return;
     });
-    describe("got to site", function () {
-      it("should have completed a connection", function (done) {
+    describe('got to site', function () {
+      it('should have completed a connection', function (done) {
         done();
       });
     });
-    describe("people form", function () {
+    describe('people form', function () {
       this.timeout(5000);
-      it("should have various elements", async function () {
+      it('should have various elements', async function () {
         this.nameField = await page.$("input[name=name]");
         this.nameField.should.not.equal(null);
         this.ageField = await page.$("input[name=age]");
@@ -48,28 +48,52 @@ chai.should();
         this.listPeople = await page.$("#listPeople");
         this.listPeople.should.not.equal(null);
       });
-      it("should create a person record given name and age", async function () {
-        await this.nameField.type("Fred");
-        await this.ageField.type("10");
+      it('should create a person record given name and age', async function () {
+        await this.nameField.type('Fred');
+        await this.ageField.type('10');
         await this.addPerson.click();
         await sleep(200);
         const resultData = await (
-          await this.resultHandle.getProperty("textContent")
+          await this.resultHandle.getProperty('textContent')
         ).jsonValue();
-        console.log("at 1, resultData is ", resultData);
-        resultData.should.include("A person record was added");
+        console.log('at 1, resultData is ', resultData);
+        resultData.should.include('A person record was added');
         const { index } = JSON.parse(resultData);
         this.lastIndex = index;
       });
-      it("should not create a person record without an age", async function () {
+      it('should not create a person record without an age', async function () {
         // your code goes here.  Hint: to clear the age field, you need the line
         // await page.$eval("#age", (el) => (el.value = "")); 
+        await this.nameField.type('Nick');
+        await page.$eval('#age', (el)=> (el.value = ''));
+        await this.addPerson.click();
+        await sleep(200);
+        const resultData = await (
+          await this.resultHandle.getProperty('textContent')
+        ).jsonValue();
+        console.log('at 2, resultData is ', resultData);
+        resultData.should.include('Please enter an age');
       });
-      it("should return the entries just created", async function () {
+      it('should return the entries just created', async function () {
          // your code goes here
+         await this.listPeople.click();
+         await sleep(200);
+         const resultData = await(
+          await this.resultHandle.getProperty('textContent')
+         ).jsonValue();
+         console.log('at 3, resultData is ', resultData);
+         resultData.should.include('Fred');
       });
-      it("should return the last entry.", async function () {
+      it('should return the last entry.', async function () {
          // your code goes here
+         await this.personIndex.type(`${this.lastIndex}`);
+         await this.getPerson.click();
+         await sleep(200);
+         const resultData = await(
+          await this.resultHandle.getProperty('textContent')
+         ).jsonValue();
+         console.log('at 4, resultData is ', resultData);
+         resultData.should.include('Fred')
       });
     });
   });
